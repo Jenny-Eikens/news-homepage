118b8ea17961dddaf9005f4864d40634169a5b52
Jenny-Eikens <jennifereikens@web.de>
2024-11-23 16:28:34 +0100

    Stop tracking assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="m-auto h-auto w-full max-w-[1200px] p-3 lg:w-[80vw] lg:p-[3rem]">
+      <div className="m-auto h-auto w-full max-w-[1200px] p-3 lg:w-[90vw] lg:p-[2rem] xl:w-[80vw] xl:p-[3rem]">
         <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <ArticleGrid />
       </div>
